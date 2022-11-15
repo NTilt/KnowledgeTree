e9c0091a945af16efd7dd82065887e7491713af4
Nikita <ya.n.2@mail.ru>
2022-11-15 23:00:57 +0400

    Initial Commit

---
+++ KnowledgeTree/KnowledgeTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowledgeTreeApp.swift
+//  KnowledgeTree
+//
+//  Created by Никита Ясеник on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct KnowledgeTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
